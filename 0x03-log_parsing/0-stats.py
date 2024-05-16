a86722856f8695ae096464ed5b0b7447f180105b
Hatim Selmun <selmun13@gmail.com>
2024-05-16 21:07:05 +0100

    log parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -8,10 +8,6 @@ import sys
 
 pattern = re.compile(r'^(\d{1,3}\.){3}\d{1,3} - \[\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}\.\d{6}\] "GET /projects/260 HTTP/1\.1" \d{3} \d+$')
 
-# pattern = re.compile(r'^(\d{1,3}\.){3}\d{1,3} - \
-#         \[\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}\.\d{6}\] \
-#         "GET /projects/260 HTTP/1\.1" \d{3} \d+$')
-
 status_code = {"200": 0, "301": 0, "400": 0, "401": 0,
                "403": 0, "404": 0, "405": 0, "500": 0}
 
@@ -20,7 +16,7 @@ line_count = 0
 
 
 def print_statistics():
-    """"""
+    """Prints the file size and status code counts"""
     print("File_size: {}".format(total_size))
     sorted_dict = dict(sorted(status_code.items(),
                               key=lambda item: item[1],
@@ -32,7 +28,7 @@ def print_statistics():
 
 
 def sigint_handler(signum, frame):
-    """"""
+    """Handles the SIGINT signal (CTRL + C)"""
     print_statistics()
     sys.exit(0)
 
