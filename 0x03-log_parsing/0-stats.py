a45c2dd82a5fa5b51c792cf920bdeb1e5150b640
Hatim Selmun <selmun13@gmail.com>
2024-05-16 21:05:59 +0100

    log parsing

---
+++ 0x03-log_parsing/0-stats.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python3
+"""Log parsing"""
+import re
+import signal
+import signal
+import sys
+
+
+pattern = re.compile(r'^(\d{1,3}\.){3}\d{1,3} - \[\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}\.\d{6}\] "GET /projects/260 HTTP/1\.1" \d{3} \d+$')
+
+# pattern = re.compile(r'^(\d{1,3}\.){3}\d{1,3} - \
+#         \[\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}\.\d{6}\] \
+#         "GET /projects/260 HTTP/1\.1" \d{3} \d+$')
+
+status_code = {"200": 0, "301": 0, "400": 0, "401": 0,
+               "403": 0, "404": 0, "405": 0, "500": 0}
+
+total_size = 0
+line_count = 0
+
+
+def print_statistics():
+    """"""
+    print("File_size: {}".format(total_size))
+    sorted_dict = dict(sorted(status_code.items(),
+                              key=lambda item: item[1],
+                              reverse=True))
+
+    for st_code, count in sorted_dict.items():
+        if count > 0:
+            print("{}: {}".format(st_code, count))
+
+
+def sigint_handler(signum, frame):
+    """"""
+    print_statistics()
+    sys.exit(0)
+
+
+signal.signal(signal.SIGINT, sigint_handler)
+
+
+try:
+    while True:
+        line = sys.stdin.readline().strip()
+        if not line:
+            break
+
+        if pattern.fullmatch(line):
+            parts = line.split()
+            file_size = int(parts[-1])
+            code = parts[-2]
+
+            total_size += file_size
+            if code in status_code:
+                status_code[code] += 1
+            line_count += 1
+
+        if line_count == 10:
+            print_statistics()
+            line_count = 0
+
+except Exception as e:
+    print("Error:", e)
+finally:
+    print_statistics()
