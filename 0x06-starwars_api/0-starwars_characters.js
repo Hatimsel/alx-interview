fa4d08e0715e402ed787ef1430550c826fdcb7be
Hatim Selmun <selmun13@gmail.com>
2024-06-06 18:05:49 +0100

    starwars api

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -6,8 +6,8 @@ const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
 
 request(url, function (error, response, body) {
   if (error) {
-	console.error('Error fetching film data:', error);
-	return;
+    console.error('Error fetching film data:', error);
+    return;
   }
 
   const characterUrls = JSON.parse(body).characters;
@@ -18,12 +18,12 @@ const fetchCharacterNamesInOrder = (urls, index) => {
   if (index === urls.length) return;
 
   request(urls[index], function (error, response, body) {
-	if (error) {
-	console.error('Error fetching character data:', error);
-	return;
-	}
+    if (error) {
+      console.error('Error fetching character data:', error);
+      return;
+    }
 
-	console.log(JSON.parse(body).name);
-	fetchCharacterNamesInOrder(urls, index + 1);
+    console.log(JSON.parse(body).name);
+    fetchCharacterNamesInOrder(urls, index + 1);
   });
 };
