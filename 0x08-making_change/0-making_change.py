98d07fd60a7ca40f355ec2b581f68337cea755b1
Hatim Selmun <selmun13@gmail.com>
2024-06-23 17:49:36 +0100

    making change interview problem

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,22 +1,25 @@
 #!/usr/bin/python3
 """Change comes from within"""
+from collections import deque
 
 
 def makeChange(coins, total):
-    """
-    Determine the fewest number of coins needed to meet
-    a given amount total
-    Return fewest number of coins needed to meet total
-    """
     if total <= 0:
         return 0
 
-    dp = [float('inf')] * (total + 1)
-    dp[0] = 0
+    queue = deque([(0, 0)])
+    visited = set([0])
 
-    for amount in range(1, total + 1):
+    while queue:
+        current_amount, coin_count = queue.popleft()
+
         for coin in coins:
-            if coin <= amount:
-                dp[amount] = min(dp[amount], dp[amount - coin] + 1)
+            new_amount = current_amount + coin
 
-    return dp[total] if dp[total] != float('inf') else -1
+            if new_amount == total:
+                return coin_count + 1
+            if new_amount < total and new_amount not in visited:
+                visited.add(new_amount)
+                queue.append((new_amount, coin_count + 1))
+
+    return -1
